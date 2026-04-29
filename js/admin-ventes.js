81a35efc40fc09c96a1b38765357658feb2a5fb7
univers-caresse <universcaresse@gmail.com>
2026-04-29 11:37:46 -0400

    Update admin-ventes.js

--- js/admin-ventes.js
+++ js/admin-ventes.js
@@ -418,7 +418,7 @@ function payerAvecSquare() {
     version: '1.3',
     notes: 'Univers Caresse'
   }));
-  alert('Montant en cents : ' + montantCents);
+  document.getElementById('fv-spinner').innerHTML += '<div>' + montantCents + '</div>';
 window.location.href = `square-commerce-v1://payment/create?data=${data}`;
 }
 
