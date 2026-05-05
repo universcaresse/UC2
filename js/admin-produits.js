9bd76ca0e06d3869ab04062e1e71a700e6f93d0f
univers-caresse <universcaresse@gmail.com>
2026-05-04 21:40:44 -0400

    Update admin-produits.js

--- js/admin-produits.js
+++ js/admin-produits.js
@@ -254,7 +254,7 @@ async function afficherProduits() {
             ? '<div class="carte-formats">' + [].concat(pro.formats).sort(function(a, b) { return parseFloat(a.poids) - parseFloat(b.poids); }).map(function(f) {
                 var qteInv = inv[f.poids + '_' + f.unite] || 0;
                 var invHTML = '<span class="carte-format-prix">(' + qteInv + ' dispo)</span>';
-                return '<div class="carte-format-tag"><span class="carte-format-prix">' + parseFloat(f.prix_vente).toFixed(2).replace('.', ',') + ' $</span><span class="carte-format-sep"></span><span class="carte-format-poids">' + f.poids + ' ' + f.unite + '</span>' + invHTML + '</div>';
+                return '<div class="carte-format-tag"><span class="carte-format-prix">' + parseFloat(f.prix_vente).toFixed(2).replace('.', ',') + ' $</span><span class="carte-format-sep"></span><span class="carte-format-prix">' + f.poids + ' ' + f.unite + '</span>' + invHTML + '</div>';
               }).join('') + '</div>'
             : '';
 
